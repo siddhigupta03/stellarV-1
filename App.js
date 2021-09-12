2570dcbfb657c8d6680891ad9f275686aa5bb9cf
siddhigupta03 <76785581+siddhigupta03@users.noreply.github.com>
2021-09-12 14:53:32 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import Home from './Screens/Home';
+import DailyPic from './Screens/DailyPic';
+import SpaceCrafts from './Screens/SpaceCrafts';
+import StarMap from './Screens/StarMap';
+
+const Stack = createStackNavigator();
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName = 'Home' screenOptions={{headerShown: false}}>
+          <Stack.Screen name='Home' component={Home}/>
+          <Stack.Screen name='Daily Pics' component={DailyPic}/>
+          <Stack.Screen name='Space Crafts' component = {SpaceCrafts}/>
+          <Stack.Screen name='Star Maps' component={StarMap}/>
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
